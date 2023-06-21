5ba679816b757f9f5923112c2dca2862e5331985
Jaroch <aleksandra.jaroch@capgemini.com>
2023-06-21 12:35:36 +0200

    Modified README

--- main.py
+++ main.py
@@ -8,27 +8,26 @@ import yaml
 
 
 def get_args():
-    """_summary_
+    """The function allows to get argparse arguments
 
-    :return: _description_
-    :rtype: _type_
+    :return: Argparse arguments
+    :rtype: argparse
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_one', type=str, required=True)
     parser.add_argument('--file_two', type=str, required=True)
-    parser.add_argument('--countries', nargs='+', help='delimited list input', required=True)
-    # for string with spaces use three double quotes '''string with space'''
+    parser.add_argument('--countries', nargs='*',  required=True)
     args = parser.parse_args()
     return args
 
 
 def get_logger(config):
-    """_summary_
+    """Create a logging object with rotating policy
 
-    :param config: _description_
-    :type config: _type_
-    :return: _description_
-    :rtype: _type_
+    :param config: A yaml file allowing to store some parameters
+    :type config: yaml
+    :return: logging object
+    :rtype: Logging
     """
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
@@ -48,15 +47,15 @@ def get_logger(config):
     return logger
 
 
-def check_paths(file_one, file_two):
-    """_summary_
+def check_files(file_one: str, file_two: str):
+    """The function checks if paths to the file exist and if the file are in .csv format
 
-    :param file_one: _description_
-    :type file_one: _type_
-    :param file_two: _description_
-    :type file_two: _type_
-    :return: _description_
-    :rtype: _type_
+    :param file_one: A path to the dataset_one file
+    :type file_one: str
+    :param file_two: A path to the dataset_two file
+    :type file_two: str
+    :return: True or false depending on if the files exist and it they csv format
+    :rtype: boolean
     """
     if os.path.exists(file_one) and os.path.exists(file_two):
         if file_one.endswith('csv') and file_two.endswith('csv'):
@@ -66,7 +65,7 @@ def check_paths(file_one, file_two):
 
 
 def main():
-    """The main function of the application.
+    """The main function of the application. 
     """
     args = get_args()
 
@@ -77,7 +76,7 @@ def main():
         
     spark_session = SparkSession.builder.appName("assignment").getOrCreate()
     
-    if check_paths(args.file_one, args.file_two):
+    if check_files(args.file_one, args.file_two):
         clients_df = DataFrameCreator(args.file_one, spark_session, logger)
         clients_df.filter_country_column(args.countries, logger)
         clients_df.drop_columns(config["drop_names"], logger)
